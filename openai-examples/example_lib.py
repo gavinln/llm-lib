3b098d6bf151b7048864e3d66515cd1763b8c8b0
Gavin Noronha <gavinln@hotmail.com>
2023-06-04 21:58:07 -0700

    move functions to separate module

---
+++ openai-examples/example_lib.py
@@ -0,0 +1,79 @@
+import os
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Load your API key from an environment variable or secret management service
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+# basic example
+def basic_example(prompt):
+    response = openai.Completion.create(
+        model="text-davinci-003", prompt=prompt, temperature=0, max_tokens=7
+    )
+    print("prompt is {}".format(prompt))
+    text = response["choices"][0]["text"]
+    print("response text is {}".format(text))
+
+
+def question_answer(prompt):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=["\n"],
+    )
+    print("prompt is {}".format(prompt))
+    text = response["choices"][0]["text"]
+    print("response text is {}".format(text))
+
+
+def correct_grammar(prompt):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Correct this to standard English:\n\nShe no went to the market.",
+        temperature=0,
+        max_tokens=60,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+    )
+    print("prompt is {}".format(prompt))
+    text = response["choices"][0]["text"]
+    print("response text is {}".format(text))
+
+
+def summarize_second_grader(prompt):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=1,
+        max_tokens=64,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+    )
+    print("prompt is {}".format(prompt))
+    text = response["choices"][0]["text"]
+    print("response text is {}".format(text))
+
+
+def translate_english(prompt):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.3,
+        max_tokens=100,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+    print("prompt is {}".format(prompt))
+    text = response["choices"][0]["text"]
+    print("response text is {}".format(text))
