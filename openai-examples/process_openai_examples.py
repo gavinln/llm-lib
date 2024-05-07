864b7741e15b9fb0b1b28f8923d34267e8960ad4
Gavin Noronha <gavinln@hotmail.com>
2024-05-06 21:47:52 -0700

    rename target

--- openai-examples/process_openai_examples.py
+++ openai-examples/process_openai_examples.py
@@ -7,7 +7,9 @@ examples.
 
 import logging
 import pathlib
+import re
 import sys
+from collections import Counter
 
 import fire
 import pandas as pd
@@ -94,7 +96,47 @@ def print_tags():
     print("new tags----------------\n", new_tags)
 
 
-def print_examples():
+def get_title_words(title: pd.Series):
+    "get word counts from titles"
+    # remove punctutation from titles
+    punctuation_re = re.compile("['\-/()<>:;,.?]+")
+    clean_titles = title.str.lower().apply(lambda s: punctuation_re.sub("", s))
+    words = " ".join(clean_titles.tolist()).split()
+    word_counts = Counter(words)
+    return word_counts
+
+
+def get_stop_words():
+    words = """
+
+    with using for and search embeddings to a vector how database as the api
+    question answering models chat function calling semantic vision completions
+    & retrieval in module data generation getting started use weights biases
+    augmented db prompt functions visualizing generative batch processing long
+    create assistants sdk new your evaluate an build agent preview philosophy
+    analysis places post-processing example document unit test writing
+    multi-step hybrid context embedding filtered vectorize summarizing
+    documents synthetic part clip improve multimodal evals moderation parse pdf
+    docs tag caption images combine clothing app implement creating slides
+    graph overview python fine tuning narrating video visual capabilities make
+    outputs consistent seed parameter named entity recognition enrich text
+    openapi specification nodejs monitoring deep lake similarity postgres
+    automate aws tasks completion own ai preparation model fine-tune
+    summarization task nearby leveraging google customer profiles addressing
+    transcription misspellings vs enhancing transcriptions pre- techniques
+    knowledge base call legacy dynamic masks segment anything running queries
+    store vectors json tool-using wikipedia articles robust augmentation format
+    inputs chatgpt content extraction q&a work large language cases related
+    resources from around web texts that are longer than models maximum length
+    count tokens
+
+    """
+    space_re = re.compile(r"\s\+")
+    return space_re.sub(" ", words).split()
+
+
+def print_examples_stats():
+    "print statistics of the openai examples"
     df = get_openai_examples_data()
     default_tags = get_default_tags()
     tag_combos = df.tags.apply(
@@ -110,6 +152,35 @@ def print_examples():
         for tag in default_tags
     }
     print(tag_counts)
+    word_counts_all = get_title_words(df.title)
+    stop_words = get_stop_words()
+    word_counts = [word for word in word_counts_all if word not in stop_words]
+    word_counts = [word for word in word_counts if not word.isnumeric()]
+
+    print(word_counts)
+
+    vector_titles = df.title[
+        df.title.str.lower().str.contains("vector")
+    ].tolist()
+    print("\n".join(vector_titles))
+
+
+def vector_in_titles():
+    "print examples with vector in title"
+    df = get_openai_examples_data()
+    vector_titles = df.title[
+        df.title.str.lower().str.contains("vector")
+    ].tolist()
+    print("\n".join(vector_titles))
+
+
+def redis_in_titles():
+    "print examples with redis in title"
+    df = get_openai_examples_data()
+    redis_titles = df.title[
+        df.title.str.lower().str.contains("redis")
+    ].tolist()
+    print("\n".join(redis_titles))
 
 
 def process_examples():
@@ -122,7 +193,9 @@ def main():
         {
             "print-tags": print_tags,
             "process-examples": process_examples,
-            "print-examples": print_examples,
+            "print-examples-stats": print_examples_stats,
+            "vector-in-titles": vector_in_titles,
+            "redis-in-titles": redis_in_titles,
         }
     )
 
