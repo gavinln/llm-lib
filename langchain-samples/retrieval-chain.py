dbb3ce90b22431ab57a42c13c234d15b71ad4645
Gavin Noronha <gavinln@hotmail.com>
2024-05-22 20:47:13 -0700

    add example for retrieval chain

--- langchain-samples/retrieval-chain.py
+++ langchain-samples/retrieval-chain.py
@@ -7,11 +7,15 @@ import pathlib
 from typing import Any
 
 from bs4 import BeautifulSoup
-from langchain_community.llms.ollama import Ollama
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.embeddings.ollama import OllamaEmbeddings
+from langchain_community.llms.ollama import Ollama
+from langchain_community.vectorstores.faiss import FAISS
+from langchain_core.documents.base import Document
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
 
 SCRIPT_DIR = pathlib.Path(__file__).parent.resolve()
 log = logging.getLogger(__name__)
@@ -60,12 +64,47 @@ def beautifulsoup_example():
     print(soup.prettify())
 
 
+def get_url_docs(url) -> list[Document]:
+    loader = WebBaseLoader(url)
+    docs = loader.load()
+    return docs
+
+
+def get_vector_store(docs) -> FAISS:
+    embeddings = OllamaEmbeddings(model="orca-mini")
+    # Recursive Character Text Splitter: default options try
+    # to keep all paragraphs, sentences, and then words together.
+    text_splitter = RecursiveCharacterTextSplitter()
+    documents = text_splitter.split_documents(docs)
+    print(f"orginal {len(docs)} documents, new {len(documents)} documents")
+    vector_store = FAISS.from_documents(documents, embeddings)
+    return vector_store
+
+
 def main():
+    # DOES NOT WORK CORRECTLY
     # beautifulsoup_example()
+    llm = Ollama(model="orca-mini")
+    query = "how can langsmith help with testing?"
+    print(llm.invoke(query))
 
     langsmith_url = "https://docs.smith.langchain.com/how_to_guides"
-    loader = WebBaseLoader(langsmith_url)
-    breakpoint()
+    docs = get_url_docs(langsmith_url)
+    assert len(docs) > 0, "cannot get documents"
+    vector_store = get_vector_store(docs)
+    print(type(vector_store))
+
+    prompt = ChatPromptTemplate.from_template("""
+    Answer the following question based only on the provided context:
+
+    <context>
+    {context}
+    </context>
+
+    Question: {input}""")
+
+    document_chain = create_stuff_documents_chain(llm, prompt)
+    print(document_chain)
 
 
 if __name__ == "__main__":
