2668f3f40a89babb82fb64d2d80c22b9e2b172eb
Gavin Noronha <gavinln@hotmail.com>
2024-08-14 14:08:12 -0700

    add gorilla llm examples

---
+++ gorilla-samples/README.md
@@ -0,0 +1,14 @@
+# Gorilla samples
+
+[Gorilla][100] is a large language model (LLM) trained to call code and make application programming interface (API) calls.
+
+[100]: https://github.com/ShishirPatil/gorilla
+
+## gorilla CLI app
+
+## Add torch CPU library
+
+```
+poetry source add --priority=explicit torch https://download.pytorch.org/whl/cpu
+poetry add --source torch torch
+```
---
+++ gorilla-samples/gorilla-functions.py
@@ -0,0 +1,194 @@
+"""
+https://github.com/ShishirPatil/gorilla/tree/main/openfunctions/openfunctions-v1
+"""
+
+import json
+import logging
+import pathlib
+import pprint as pp
+
+import fire
+import openai
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
+SCRIPT_DIR = pathlib.Path(__file__).parent.resolve()
+log = logging.getLogger(__name__)
+
+
+def get_gorilla_response(
+    query,
+    model,
+    functions=[],
+):
+    openai.api_key = "EMPTY"
+    openai.api_base = "http://luigi.millennium.berkeley.edu:8000/v1"
+    try:
+        completion = openai.ChatCompletion.create(
+            model="gorilla-openfunctions-v1",
+            temperature=0.0,
+            messages=[{"role": "user", "content": query}],
+            functions=functions,
+        )
+        return completion.choices[0].message.content  # type: ignore
+    except Exception as e:
+        print(e, model, query)
+
+
+def get_uber_ride_function() -> dict:
+    return {
+        "name": "Uber Carpool",
+        "api_name": "uber.ride",
+        "description": "Find suitable ride for customers given the "
+        + "location, type of ride, and the amount of time the customer "
+        + "is willing to wait as parameters",
+        "parameters": [
+            {
+                "name": "loc",
+                "description": "location of the starting place of the "
+                + "uber ride",
+            },
+            {
+                "name": "type",
+                "enum": ["plus", "comfort", "black"],
+                "description": "types of uber ride user is ordering",
+            },
+            {
+                "name": "time",
+                "description": "the amount of time in minutes the "
+                + "customer is willing to wait",
+            },
+        ],
+    }
+
+
+def get_recursive_size(root_dir) -> int:
+    root_path = pathlib.Path(root_dir)
+    return sum(file.stat().st_size for file in root_path.rglob("*"))
+
+
+def get_hub_model_sizes(cache_dir):
+    cache_path = pathlib.Path(cache_dir)
+    hub_dir = cache_path / "hub"
+    hub_models = [f for f in hub_dir.glob("model*") if f.is_dir()]
+    return {model: get_recursive_size(model) for model in hub_models}
+
+
+def show_huggingface_cached_models():
+    "show huggingface cached models"
+    cache_dir = pathlib.Path("~") / ".cache" / "huggingface"
+    cache_dir = cache_dir.expanduser().resolve()
+    model_size_dict = get_hub_model_sizes(cache_dir)
+    size_name_dict = {
+        f"{size:15,d}": p.name for p, size in model_size_dict.items()
+    }
+    print(
+        "\n".join(size + " " + name for size, name in size_name_dict.items())
+    )
+    return ""
+
+
+def hosted_functions():
+    "run gorilla function call on hosted model"
+    model = "gorilla-openfunctions-v0"
+    query = (
+        'Call me an Uber ride type "Plus" in Berkeley at zipcode '
+        + "94704 in 10 minutes"
+    )
+    functions = [get_uber_ride_function()]
+    out = get_gorilla_response(query, model, functions=functions)
+    print(out)
+
+
+def get_prompt(user_query: str, functions: list = []) -> str:
+    """
+    Generates a conversation prompt based on the user's query and a list of functions.
+
+    Parameters:
+    - user_query (str): The user's query.
+    - functions (list): A list of functions to include in the prompt.
+
+    Returns:
+    - str: The formatted conversation prompt.
+    """
+    if len(functions) == 0:
+        return f"USER: <<question>> {user_query}\nASSISTANT: "
+    functions_string = json.dumps(functions)
+    return f"USER: <<question>> {user_query} <<function>> {functions_string}\nASSISTANT: "
+
+
+def local_functions():
+    "run gorilla function call on local torch model"
+    # Device setup
+    device: str = "cuda:0" if torch.cuda.is_available() else "cpu"
+    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+    # Model and tokenizer setup
+    model_id: str = "gorilla-llm/gorilla-openfunctions-v1"
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True
+    )
+
+    # Move model to device
+    model.to(device)
+
+    # Pipeline setup
+    pipe = pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        max_new_tokens=128,
+        batch_size=16,
+        torch_dtype=torch_dtype,
+        device=device,
+    )
+
+    # Example usage
+    query: str = (
+        'Call me an Uber ride type "Plus" in Berkeley at zipcode 94704 in 10 minutes'
+    )
+    functions = [
+        {
+            "name": "Uber Carpool",
+            "api_name": "uber.ride",
+            "description": "Find suitable ride for customers given the location, type of ride, and the amount of time the customer is willing to wait as parameters",
+            "parameters": [
+                {
+                    "name": "loc",
+                    "description": "Location of the starting place of the Uber ride",
+                },
+                {
+                    "name": "type",
+                    "enum": ["plus", "comfort", "black"],
+                    "description": "Types of Uber ride user is ordering",
+                },
+                {
+                    "name": "time",
+                    "description": "The amount of time in minutes the customer is willing to wait",
+                },
+            ],
+        }
+    ]
+    functions = [get_uber_ride_function()]
+
+    # Generate prompt and obtain model output
+    prompt = get_prompt(query, functions=functions)
+    output = pipe(prompt)
+    pp.pprint(output)
+
+
+def main():
+    fire.Fire(
+        {
+            "hosted-functions": hosted_functions,
+            "local-functions": local_functions,
+            "show-huggingface-cached-models": show_huggingface_cached_models,
+        }
+    )
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.WARN)
+    # logging.basicConfig(level=logging.DEBUG)
+    main()
