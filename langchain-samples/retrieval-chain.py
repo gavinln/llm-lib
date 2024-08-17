35866f2af43ba99229eb7ac74a5c69cee73339b1
Gavin Noronha <gavinln@hotmail.com>
2024-08-16 21:11:22 -0700

    add retrieval chain example (RAG)

--- langchain-samples/retrieval-chain.py
+++ langchain-samples/retrieval-chain.py
@@ -4,22 +4,28 @@ https://python.langchain.com/v0.1/docs/get_started/quickstart/
 
 import logging
 import pathlib
+import tempfile
 from typing import Any
 
 from bs4 import BeautifulSoup
-from langchain.chains import create_retrieval_chain
+from joblib import Memory
+from langchain.chains import (
+    create_history_aware_retriever,
+    create_retrieval_chain,
+)
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.document_loaders import WebBaseLoader
-from langchain_community.embeddings.ollama import OllamaEmbeddings
-from langchain_community.llms.ollama import Ollama
-from langchain_community.vectorstores.faiss import FAISS
-from langchain_core.documents.base import Document
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.vectorstores import VectorStore
+from langchain_community.vectorstores import FAISS
+from langchain_core.documents import Document
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 SCRIPT_DIR = pathlib.Path(__file__).parent.resolve()
 log = logging.getLogger(__name__)
+
+memory = Memory(tempfile.gettempdir(), verbose=0)
 
 
 def example_html_doc():
@@ -65,61 +71,112 @@ def beautifulsoup_example():
     print(soup.prettify())
 
 
-def get_url_docs(url) -> list[Document]:
+@memory.cache
+def llm_query_no_rag(query: str):
+    llm = ChatOpenAI()
+    query = "how can langsmith help with testing?"
+    return llm.invoke(query)
+
+
+@memory.cache
+def get_web_documents(url: str) -> list[Document]:
     loader = WebBaseLoader(url)
     docs = loader.load()
     return docs
 
 
-def get_vector_store(docs) -> VectorStore:
-    embeddings = OllamaEmbeddings(model="orca-mini")
-    # Recursive Character Text Splitter: default options try
-    # to keep all paragraphs, sentences, and then words together.
+@memory.cache
+def get_split_documents(docs: list[Document]):
     text_splitter = RecursiveCharacterTextSplitter()
+    print("original docs length: {:>12,d}".format(len(docs[0].page_content)))
     documents = text_splitter.split_documents(docs)
-    print(f"orginal {len(docs)} documents, new {len(documents)} documents")
+    for doc in documents:
+        print("   split docs length: {:>12,d}".format(len(doc.page_content)))
+    return documents
+
+
+def get_docs_vector(docs: list[Document]):
+    documents = get_split_documents(docs)
+    embeddings = OpenAIEmbeddings()
     vector_store = FAISS.from_documents(documents, embeddings)
     return vector_store
 
 
-def get_retrieval_chain(vector_store: VectorStore, document_chain):
-    retriever = vector_store.as_retriever()
-    retrieval_chain = create_retrieval_chain(retriever, document_chain)
-    return retrieval_chain
+def example_document_chain_invoke(document_chain: Any, query: str):
+    message: str = document_chain.invoke(
+        {
+            "input": query,
+            "context": [
+                Document(
+                    page_content="langsmith can let you visualize test results"
+                )
+            ],
+        }
+    )
+    print(message)
 
 
 def main():
     # beautifulsoup_example()
-    llm = Ollama(model="orca-mini")
-    query = "how can langsmith help with testing?"
-    print(llm.invoke(query))
+    llm = ChatOpenAI()
+    query = "How can langsmith help with testing? Be concise."
+    message = llm_query_no_rag(query)
+    print("llm_query_no_rag", "-" * 10)
+    print(message.content)
 
-    langsmith_url = "https://docs.smith.langchain.com/how_to_guides"
-    docs = get_url_docs(langsmith_url)
-    assert len(docs) > 0, "cannot get documents"
-    vector_store = get_vector_store(docs)
-    print(type(vector_store))
+    url = "https://docs.smith.langchain.com/user_guide"
+    docs = get_web_documents(url)
+    assert len(docs) > 0, "Cannot get documents"
+    vector_store = get_docs_vector(docs)
 
     prompt = ChatPromptTemplate.from_template(
         """
-    Answer the following question based only on the provided context:
+        Answer the following question based only on the provided context:
+            <context>
+            {context}
+            </context>
 
-    <context>
-    {context}
-    </context>
-
-    Question: {input}
+            Question: {input}
     """
     )
-
     document_chain = create_stuff_documents_chain(llm, prompt)
-    retrieval_chain = get_retrieval_chain(vector_store, document_chain)
-    response = retrieval_chain.invoke({"input": query})
-    print(f"{response['input']=}")
-    print(f"{response['answer']=}")
-    for idx, context in enumerate(response["context"]):
-        print(f"--context {idx}-----------------------")
-        print(context.page_content)
+    # example_document_chain_invoke(document_chain, query)
+
+    retriever = vector_store.as_retriever()
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+    response_dict = retrieval_chain.invoke({"input": query})
+    print(f"{response_dict['input']=}")
+    print(f"{response_dict['answer']=}")
+    for idx, context in enumerate(response_dict["context"]):
+        print(f"context {idx}", "-" * 10)
+        print("context length: {:>12,d}".format(len(context.page_content)))
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("user", "{input}"),
+            (
+                "user",
+                "Given the above conversation, generate a search query "
+                + "to look up to get information relevant to the conversation",
+            ),
+        ]
+    )
+    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+
+    chat_history = [
+        HumanMessage(
+            content="Can LangSmith help test my LLM applications? Be concise."
+        ),
+        AIMessage(content="Yes!"),
+    ]
+    response_list = retriever_chain.invoke(
+        {"chat_history": chat_history, "input": "Tell me how. Be concise."}
+    )
+    for idx, response in enumerate(response_list):
+        print(f"response {idx}")
+        print(f"response length {len(response.page_content)}")
 
 
 if __name__ == "__main__":
